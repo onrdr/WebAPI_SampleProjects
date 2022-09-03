28e86a5a9f7dc0c9158b6327a2611878dcf7ad66
onrdrmn <onrdrmn@gmail.com>
2022-09-04 01:20:21 +0300

    API controller methods added.

---
+++ Notes/NotesAPI/Controllers/NotesController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NotesAPI.Data;
+using NotesAPI.Models.Entities;
+
+namespace NotesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class NotesController : Controller
+    {
+        private readonly NotesDbContext _dbContext;
+        public NotesController(NotesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllNotes()
+        {
+            return Ok(await _dbContext.Notes.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        [ActionName("GetNoteById")]
+        public async Task<IActionResult> GetNoteById([FromRoute] Guid id)
+        {            
+            var note = await _dbContext.Notes.FindAsync(id);
+            // We also can use =>var note = await _dbContext.Notes.FirstOrDefaultAsync(n => n.Id == id);
+
+            if (note == null)
+                return NotFound();
+
+            return Ok(note);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddNote(Note note)
+        {
+            note.Id = Guid.NewGuid();
+            await _dbContext.Notes.AddAsync(note);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetNoteById), new { id = note.Id }, note);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateNote([FromRoute] Guid id, [FromBody] Note updatedNote)
+        {
+            var existingNote = await _dbContext.Notes.FindAsync(id);
+            if (existingNote == null)
+            {
+                return NotFound();
+            }
+
+            existingNote.Title = updatedNote.Title;
+            existingNote.Description = updatedNote.Description;
+            existingNote.IsVisible = updatedNote.IsVisible;
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(existingNote);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteNote([FromRoute] Guid id)
+        {
+            var existingNote = await _dbContext.Notes.FindAsync(id);
+            if (existingNote == null)
+            {
+                return NotFound();
+            }
+
+           _dbContext.Notes.Remove(existingNote);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
---
+++ Notes/NotesAPI/Data/NotesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesAPI.Models.Entities;
+
+namespace NotesAPI.Data
+{
+    public class NotesDbContext : DbContext
+    {
+        public NotesDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
